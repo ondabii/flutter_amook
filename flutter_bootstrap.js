1923ed2610c42b1cdb2d7b871b137ea9cfcc38ad
ondabii <ondabii@kakao.com>
2025-12-03 00:02:19 +0900

    tablet handle

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3771775677"
+    serviceWorkerVersion: "2876114936"
   }
 });
