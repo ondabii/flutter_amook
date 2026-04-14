44598a289dd54cf88b35412605824d51b73abfb6
ondabii <ondabii@kakao.com>
2026-04-14 16:13:54 +0900

    font add

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2036774209"
+    serviceWorkerVersion: "1848519336"
   }
 });
